1bc6fc0fd5ac0a1145a692c86c9895b21be82968
moshabbir-dotcom <moshabbir@ymail.com>
2022-01-11 15:33:15 +0000

    add contact page

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,7 +1,8 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('contact.html/', views.contact, name='contact'),
 ]
