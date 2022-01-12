aba295bd7c1a1fe84a4cff05a19ed79997f2a509
moshabbir-dotcom <moshabbir@ymail.com>
2022-01-12 11:48:45 +0000

    add img static tags and content for home.html

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact.html', views.contact, name='contact'),
-    path('about.html', views.about, name='about'),
-    path('prices.html', views.prices, name='prices'),
+    path('contact.html/', views.contact, name='contact'),
+    path('about.html/', views.about, name='about'),
+    path('prices.html/', views.prices, name='prices'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 
 # Create your views here.
+
 def home(request):
     return render(request, 'home.html')
 
