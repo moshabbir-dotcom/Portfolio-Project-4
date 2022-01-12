a363702b62b6eb72e19fdee24c0b071ec11c28bf
moshabbir-dotcom <moshabbir@ymail.com>
2022-01-12 20:57:19 +0000

    set client db model and migrate

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Clients(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    passwrd = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.fname + ' ' + self.lname
