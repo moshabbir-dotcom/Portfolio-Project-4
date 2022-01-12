efd84438a9d5f171c910ce6a79286d157d21eb94
moshabbir-dotcom <moshabbir@ymail.com>
2022-01-12 19:34:34 +0000

    add contact and booking response pages

--- mainsite/views.py
+++ mainsite/views.py
@@ -21,18 +21,31 @@ def booking_form(request):
 def successful_submission(request):
     return render(request, 'successful_submission.html')
 
+def provisional_booking(request):
+    return render(request, 'provisional_booking.html')
+
 def contact(request):
     if request.method == "POST":
         fname = request.POST['fname']
         lname = request.POST['lname']
         email = request.POST['email']
-        subject = request.POST['subject']
+        number = request.POST['number']
         message = request.POST['message']
 
-        # NEED TO SEND AN EMAIL
-        #send_mail()
+        # NEED TO SEND AN EMAIL FOR SUCCESSFUL CONTACT
+        successful_contact = 'Name: " + fname + lname " Phone Number: " + number + " Email: " + email + " Message: " + message'
+        
+        send_mail(
+            'Contact Form Recieved', #Subject
+            successful_contact, #Content
+            ['email'], #Recipient
+        )
 
-        return render(request, 'successful_submission.html', {'fname': fname})
+        return render(request, 'successful_submission.html', {
+            'fname': fname,
+            'lname': lname,
+            'email': email,
+            })
 
     else:
         return render(request, 'contact.html')
@@ -43,14 +56,19 @@ def booking(request):
         fname = request.POST['fname']
         lname = request.POST['lname']
         email = request.POST['email']
-        date = request.POST['date']
-        time = request.POST['time']
+        date = request.POST['']
+        time = request.POST['']
         addinfo = request.POST['addinfo']
 
         # NEED TO SEND A CONFIRMATION EMAIL TO USER AND ADMIN
         #send_mail()
 
-        return render(request, 'successful_submission.html', {'fname': fname})
+        return render(request, 'provisional_booking.html', {
+            'fname': fname,
+            'email': email,
+            'date': date,
+            'time': time,
+            })
 
     else:
         return render(request, 'booking_form.html')
