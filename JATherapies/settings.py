6200eeadbfa3030d040af0852afb540e4f7a9c46
moshabbir-dotcom <moshabbir@ymail.com>
2022-03-29 10:29:30 +0000

    rectify error 500

--- JATherapies/settings.py
+++ JATherapies/settings.py
@@ -171,10 +171,15 @@ CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings to be updated when going live in env.py
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = bool(os.environ.get('EMAIL_USE_TLS'))
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'example@jatherapies.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
